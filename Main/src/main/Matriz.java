bdb87ed8da46e15fac203dcb0c192c5bf38b3e2d
Alfred024 <21030761tcelaya.edu.mx>
2022-09-10 18:54:34 -0500

    Lo voy a dejar así porque tengo que subir la pendejada de cálculo

--- Main/src/main/Matriz.java
+++ Main/src/main/Matriz.java
@@ -10,7 +10,22 @@ public class Matriz {
     }
     
     public void adyacencia(Object matriz1[][], Object matriz2[][]){
+        int f1=0, f2=0, c1=0, c2=0;
+        Object newMatriz[][] = new Object[matriz.length][matriz.length];
         
+        if(esValida(matriz1) && esValida(matriz2)){
+            
+            for (f1 = 0; f1 < matriz.length; f1++) {
+                
+            }
+            
+        }else{
+            System.out.println("Dimensiones inválidas");
+        }
         
+    }
+    
+    public boolean esValida(Object matriz[][]){
+        return matriz.length>1;
     }
 }
