00dcbebc14eb87ca28f2948d693bb3398a07f22b
Alfred024 <21030761tcelaya.edu.mx>
2022-09-10 18:43:17 -0500

    Creación proyecto Matriz

---
+++ Main/src/main/Matriz.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Matriz {
+    
+    int dimensiones;
+    Object matriz[][];
+
+    public Matriz(int dimensiones) {
+       this.matriz = new Object[dimensiones][dimensiones];
+    }
+    
+    public void adyacencia(Object matriz1[][], Object matriz2[][]){
+        
+        
+    }
+}
